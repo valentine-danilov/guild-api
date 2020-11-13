b914c5f0470d61557ede21ed067938e0f1c09653
Valiantsin Danilau <Valiantsin_Danilau@epam.com>
2020-11-13 14:48:25 +0300

    Remove redundant files

--- back-end/src/main/java/by/danilov/constant/GeneralConstants.java
+++
@@ -1,9 +0,0 @@
-package by.danilov.constant;
-
-public abstract class GeneralConstants {
-
-    public static final String MODIFIED = "modified";
-    public static final String DELETED = "deleted";
-    public static final String CREATED = "created";
-
-}
--- back-end/src/main/java/by/danilov/repository/MusiciansRepository.java
+++
@@ -1,9 +0,0 @@
-package by.danilov.repository;
-
-import by.danilov.domain.Musician;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "musicians", path = "musicians")
-public interface MusiciansRepository extends PagingAndSortingRepository<Musician, Integer> {
-}
